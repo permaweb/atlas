b96c591f9d6e31de102078178c50534a2e9f1cf1
Rani Elhusseini <charmful0x@gmail.com>
2025-11-27 13:20:29 +0100

    feat: support all oracles single data fetch + tests

--- crates/common/src/gql.rs
+++ crates/common/src/gql.rs
@@ -9,15 +9,19 @@ pub enum Oracle {
     USDS,
     DAI,
     STETH,
+    All,
     Unknown,
 }
 
 impl Oracle {
     pub fn resolve(&self) -> String {
         match self {
-            &Oracle::USDS => USDS_ORACLE_PID.to_string(),
-            &Oracle::DAI => DAI_ORACLE_PID.to_string(),
-            &Oracle::STETH => STETH_ORACLE_PID.to_string(),
+            &Oracle::USDS => format!("[\"{}\"]", USDS_ORACLE_PID),
+            &Oracle::DAI => format!("[{}]", DAI_ORACLE_PID),
+            &Oracle::STETH => format!("[{}]", STETH_ORACLE_PID),
+            &Oracle::All => {
+                format!("[\"{USDS_ORACLE_PID}\", \"{DAI_ORACLE_PID}\", \"{STETH_ORACLE_PID}\"]")
+            }
             &Oracle::Unknown => String::new(),
         }
     }
@@ -28,7 +32,7 @@ pub struct OracleStakers {
     pub oracle: Oracle,
     query: Option<Value>,
     server_resp: Option<Value>,
-    id: Option<String>,
+    id: Option<Vec<String>>,
 }
 
 impl OracleStakers {
@@ -52,6 +56,12 @@ impl OracleStakers {
                 server_resp: None,
                 id: None,
             },
+            "all" => OracleStakers {
+                oracle: Oracle::All,
+                query: None,
+                server_resp: None,
+                id: None,
+            },
             _ => OracleStakers {
                 oracle: Oracle::Unknown,
                 query: None,
@@ -66,15 +76,17 @@ impl OracleStakers {
             return Err(anyhow!("error: unknown oracle type"));
         };
 
+        let first_var = if self.oracle != Oracle::All { 1 } else { 3 };
+
         let template = r#"
             query GetDetailedTransactions {
     transactions(
-        first: 1
+        first: $firstvar
         sort: HEIGHT_DESC
         owners: ["$ownervar"]
         tags: [
         { name: "Action", values: ["Set-Balances"] },
-        {name: "From-Process", values: ["$oraclevar"]}
+        {name: "From-Process", values: $oraclevar }
         ]
     ) {
         edges {
@@ -103,6 +115,7 @@ impl OracleStakers {
 
         // formatting as arweave.net doesnt support dynamic vars on server level
         let query = template
+            .replace("$firstvar", &first_var.to_string())
             .replace("$ownervar", AO_AUTHORITY)
             .replace("$oraclevar", &self.oracle.resolve());
 
@@ -133,35 +146,47 @@ impl OracleStakers {
         self.server_resp = Some(res);
         Ok(self)
     }
-    pub fn id(&mut self) -> Result<String, Error> {
+    pub fn id(&mut self) -> Result<Vec<String>, Error> {
         if self.id.is_none() {
-            let _ = self.set_id();
+            self.set_id()?;
         }
 
-        Ok(self
+        self.id
             .clone()
-            .id
-            .ok_or(anyhow!("error while retrieving the message id"))?)
+            .ok_or(anyhow!("error while retrieving the message id"))
     }
-    fn set_id(&mut self) -> Result<String, Error> {
+    fn set_id(&mut self) -> Result<Vec<String>, Error> {
         if self.id.is_some() {
             return Err(anyhow!("error: message id is already set"));
         };
         let res = self.server_resp.clone().ok_or(anyhow!(
             "error: no gql server response was made successfully"
         ))?;
-        let msg_id = res
+        let edges = res
             .get("data")
             .and_then(|v| v.get("transactions"))
             .and_then(|v| v.get("edges"))
-            .and_then(|v| v.get(0))
-            .and_then(|v| v.get("node"))
-            .and_then(|v| v.get("id"))
-            .and_then(|v| v.as_str())
-            .ok_or(anyhow!("error: no ao message id found for the given query"))?;
+            .and_then(|v| v.as_array())
+            .ok_or(anyhow!(
+                "error: no ao message edges found for the given query"
+            ))?;
 
-        self.id = Some(msg_id.to_string());
-        Ok(msg_id.to_string())
+        let ids: Vec<String> = edges
+            .iter()
+            .filter_map(|edge| {
+                edge.get("node")
+                    .and_then(|node| node.get("id"))
+                    .and_then(|id| id.as_str())
+                    .map(|s| s.to_string())
+            })
+            .collect();
+
+        if ids.is_empty() {
+            return Err(anyhow!("error: no ao message id found for the given query"));
+        }
+
+        self.id = Some(ids.clone());
+        Ok(ids)
     }
 }
 
@@ -169,11 +194,21 @@ impl OracleStakers {
 mod test {
     use crate::gql::OracleStakers;
     #[test]
-    fn test_oracle_stakers() {
+    fn test_single_oracle_usds_stakers() {
         let mut oracle = OracleStakers::new("usds");
-        let res = oracle.build().unwrap().send().unwrap();
+        let _res = oracle.build().unwrap().send().unwrap();
         let id = oracle.id().unwrap();
-        println!("ORACLE ID: {id}");
-        assert_eq!(id.len(), 43);
+        println!("ORACLE ID: {:?}", id);
+        assert_eq!(id.len(), 1);
+    }
+    #[test]
+    fn test_all_oracle_stakers() {
+        let mut oracle = OracleStakers::new("all");
+        let _res = oracle.build().unwrap().send().unwrap();
+        // noticied arweave gql gateway behavior is returning IDs in this order:
+        // USDS / STETH / DAI
+        let id = oracle.id().unwrap();
+        println!("ORACLE IDS: {:?}", id);
+        assert_eq!(id.len(), 3);
     }
 }
