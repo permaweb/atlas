140eeb2538762f4c0c8c8f1e34115ba9f2ef5415
Rani Elhusseini <charmful0x@gmail.com>
2025-12-06 11:25:29 +0100

    chore: add token metadata to Project

--- crates/common/src/projects.rs
+++ crates/common/src/projects.rs
@@ -18,43 +18,78 @@ pub const PIXL_PID: &str = "3eZ6_ry6FD9CB58ImCQs6Qx_rJdDUGhz-D2W1AqzHD8";
 pub const VELA_PID: &str = "8TRsYFzbhp97Er5bFJL4Xofa4Txv4fv8S0szEscqopU";
 pub const INF_PID: &str = "LnFIQUwAdMZ9LEWlfQ7VZ3zJOW-0p8Irc_2gAVshs3w";
 
+// projects tokens
+pub const PI_TOKEN: &str = "4hXj_E-5fAKmo4E8KjgQvuDJKAFk9P2grhycVmISDLs";
+pub const LOAD_TOKEN: &str = "gx_jKk-hy8-sB4Wv5WEuvTTVyIRWW3We7rRHthcohBQ";
+pub const APUS_TOKEN: &str = "mqBYxpDsolZmJyBdTK8TJp_ftOuIUXVYcSQ8MYZdJg0";
+pub const BOTG_TOKEN: &str = "Nx-_Ichdp-9uO_ZKg2DLWPiRlg-DWrSa2uGvINxOjaE";
+pub const AOS_TOKEN: &str = "GegJSRSQptBJEF5lcr4XEqWLYFUnNr3_zKQ-P_DnDQs";
+pub const WNDR_TOKEN: &str = "7GoQfmSOct_aUOWKM4xbKGg6DzAmOgdKwg8Kf-CbHm4";
+pub const ACTION_TOKEN: &str = "OiNYKJ16jP7uj7z0DJO7JZr9ClfioGacpItXTn9fKn8";
+pub const SMONEY_TOKEN: &str = "K59Wi9uKXBQfTn3zw7L_t-lwHAoq3Fx-V9sCyOY3dFE";
+pub const LQD_TOKEN: &str = "n2MhPK0O3yEvY2zW73sqcmWqDktJxAifJDrri4qireI";
+pub const GAME_TOKEN: &str = "s6jcB3ctSbiDNwR-paJgy5iOAhahXahLul8exSLHbGE";
+pub const NAU_TOKEN: &str = "5IrQh9aoWTLlLTXogXdGd7FcVubFKOaw7NCRGnkyXCM";
+pub const RELLA_TOKEN: &str = "aKmI800gM1Gk12JvwBe2MPxAvXT1ZPfRBxmkUpLJv7g";
+pub const ARIO_TOKEN: &str = "qNvAoz0TgcH7DMg8BCVn8jF32QH5L6T29VjHxhHqqGE";
+pub const PIXL_TOKEN: &str = "DM3FoZUq_yebASPhgd8pEIRIzDW6muXEhxz5-JwbZwo";
+pub const VELA_TOKEN: &str = "kfq7JKVeu-Z9qA0y-0YKXbgNqKJzENqVl0KSrPDOBl4";
+pub const INF_TOKEN: &str = "Y2ocP2gBrn4AtodCi1IyoA0X1jCJtx_aKeJddnrHb5U";
+
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Project {
     pub name: String,
     pub ticker: String,
     pub pid: String,
-    // todo! add more metadata
+    pub token: String,
+    pub denomination: u32, // todo! add more metadata
 }
 
 macro_rules! project {
-    ($fn_name:ident, $name:expr, $ticker:expr, $pid:expr) => {
+    ($fn_name:ident, $name:expr, $ticker:expr, $pid:expr, $token:expr, $denomination:expr) => {
         pub fn $fn_name() -> Project {
             Project {
                 name: $name.into(),
                 ticker: $ticker.into(),
                 pid: $pid.into(),
+                token: $token.into(),
+                denomination: $denomination.into(),
             }
         }
     };
 }
 
 impl Project {
-    project!(pi, "Permaweb Index", "PI", PI_PID);
-    project!(load, "Load Network", "LOAD", LOAD_PID);
-    project!(apus, "Apus Network", "APUS", APUS_PID);
-    project!(botega, "Botega Token", "BOTG", BOTG_PID);
-    project!(aos, "AO Strategy", "AOS", AOS_PID);
-    project!(wndr, "Wander", "WNDR", WNDR_PID);
-    project!(action, "Action", "ACTION", ACTION_PID);
-    project!(space, "Space Money", "SMONEY", SMONEY_PID);
-    project!(lqd, "Liquid Ops", "LQD", LQD_PID);
-    project!(game, "ArcAO", "GAME", GAME_PID);
-    project!(nau, "Nau", "NAU", NAU_PID);
-    project!(rella, "LLAMMA REBORN", "RELLA", RELLA_PID);
-    project!(ario, "AR.IO", "ARIO", ARIO_PID);
-    project!(pixl, "PIXL Token", "PIXL", PIXL_PID);
-    project!(vela, "Vela", "VELA", VELA_PID);
-    project!(inf, "Influence Market", "INF", INF_PID);
+    project!(pi, "Permaweb Index", "PI", PI_PID, PI_TOKEN, 12u32);
+    project!(load, "Load Network", "LOAD", LOAD_PID, LOAD_TOKEN, 18u32);
+    project!(apus, "Apus Network", "APUS", APUS_PID, APUS_TOKEN, 12u32);
+    project!(botega, "Botega Token", "BOTG", BOTG_PID, BOTG_TOKEN, 18u32);
+    project!(aos, "AO Strategy", "AOS", AOS_PID, AOS_TOKEN, 18u32);
+    project!(wndr, "Wander", "WNDR", WNDR_PID, WNDR_TOKEN, 18u32);
+    project!(action, "Action", "ACTION", ACTION_PID, ACTION_TOKEN, 18u32);
+    project!(
+        space,
+        "Space Money",
+        "SMONEY",
+        SMONEY_PID,
+        SMONEY_TOKEN,
+        18u32
+    );
+    project!(lqd, "Liquid Ops", "LQD", LQD_PID, LQD_TOKEN, 18u32);
+    project!(game, "ArcAO", "GAME", GAME_PID, GAME_TOKEN, 18u32);
+    project!(nau, "Nau", "NAU", NAU_PID, NAU_TOKEN, 18u32);
+    project!(
+        rella,
+        "LLAMMA REBORN",
+        "RELLA",
+        RELLA_PID,
+        RELLA_TOKEN,
+        18u32
+    );
+    project!(ario, "AR.IO", "ARIO", ARIO_PID, ARIO_TOKEN, 6u32);
+    project!(pixl, "PIXL Token", "PIXL", PIXL_PID, PIXL_TOKEN, 6u32);
+    project!(vela, "Vela", "VELA", VELA_PID, VELA_TOKEN, 18u32);
+    project!(inf, "Influence Market", "INF", INF_PID, INF_TOKEN, 18u32);
     // todo! add more active FLPs if any
 }
 
