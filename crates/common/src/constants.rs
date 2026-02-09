f136d6337e2974a45ad8756eceb28535c606a57d
Rani Elhusseini <charmful0x@gmail.com>
2026-02-09 16:23:13 +0100

    chore: switch back gateway

--- crates/common/src/constants.rs
+++ crates/common/src/constants.rs
@@ -19,4 +19,4 @@ pub const PI_TOKEN_START: u32 = 1_638_421;
 pub const DATA_PROTOCOL_A_START: u32 = 1_594_020; // Jan 22 2025
 pub const DATA_PROTOCOL_B_START: u32 = 1_616_999; // Feb 25 2025
 // endpoints
-pub const ARWEAVE_GATEWAY: &str = "https://frostor.xyz"; // "https://frostor.xyz";
+pub const ARWEAVE_GATEWAY: &str = "https://arweave.net"; // "https://frostor.xyz";
