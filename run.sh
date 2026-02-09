b18370cb67bdab395f1704082ed3d341157e3c66
Rani Elhusseini <charmful0x@gmail.com>
2026-02-09 12:44:05 +0100

    chore: disable indexer

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-(cd crates/indexer && cargo run --bin indexer) &
-INDEXER_PID=$!
+# (cd crates/indexer && cargo run --bin indexer) &
+# INDEXER_PID=$!
 
-trap "kill $INDEXER_PID" EXIT
+# trap "kill $INDEXER_PID" EXIT
 
 cd crates/server
 cargo run
