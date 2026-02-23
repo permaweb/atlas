9e639ba75835a8f6b0363ce32b85eae2f83d9408
Rani Elhusseini <charmful0x@gmail.com>
2026-02-23 15:09:58 +0100

    feat: add mainnet oracles ProcIDs

--- crates/common/src/constants.rs
+++ crates/common/src/constants.rs
@@ -9,6 +9,12 @@ pub const STETH_ORACLE_PID: &str = "wJV8FMkpoeLsTjJ6O7YZEuQgMqj-sDjPHhTeA73RsCc"
 pub const STETH_STAKING_ADDRESS: &str = "0xfe08d40eee53d64936d3128838867c867602665c";
 pub const DAI_ORACLE_PID: &str = "5q8vpzC5QAKOAJFM26MAKfZw1gwtw7WA_J2861ZiKhI";
 pub const DAI_STAKING_ADDRESS: &str = "0x6a1b588b0684dace1f53c5820111f400b3dbfebf";
+// FLP system yield oracle processes -- ao.N.1
+pub const USDS_ORACLE_MAINNET_PID: &str = "JJPMirAJb2RR7mqIAilAGWjA3EwHIEXAnc__CfNKqNs";
+pub const STETH_ORACLE_MAINNET_PID: &str = "U4IrjxcKVsEya5kQbZPbjLCoj868P129Z4IlArMOzuc";
+pub const DAI_ORACLE_MAINNET_PID: &str = "QzWis3AEZTl1se17kvHXko-dfhPdWXJezhuyy0e3NTg";
+// ao protocol -- mainnet
+pub const FLP_AUTHORITY_MAINNET: &str = "XRDPy6e5zYaQ74oVESYZYz9DBwucohcdgRIYplVBRQE";
 // ao protocol -- legacy
 pub const AO_AUTHORITY: &str = "fcoN_xJeisVsPXA-trzVAuIiqO3ydLQxM-L4XbrQKzY";
 pub const DELEGATION_PID: &str = "cuxSKjGJ-WDB9PzSkVkVVrIBSh3DrYHYz44usQOj5yE";
